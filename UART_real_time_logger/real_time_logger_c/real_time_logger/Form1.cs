aae6c372780e486c337589c32935b397533cd4ba
melike işeri <86420860+melike162@users.noreply.github.com>
2025-10-03 17:59:05 +0300

    Update Form1.cs

--- UART_real_time_logger/real_time_logger_c/real_time_logger/Form1.cs
+++ UART_real_time_logger/real_time_logger_c/real_time_logger/Form1.cs
@@ -27,12 +27,6 @@ namespace real_time_logger
         public Form1()
         {
             InitializeComponent();
-            /*
-            formsPlot = new ScottPlot.WinForms.FormsPlot();
-            formsPlot.Location = new Point(506, 111);
-            formsPlot.Size = new Size(600, 461);
-            this.Controls.Add(formsPlot);
-            */
         }
 
         private void btn_exit_Click(object sender, EventArgs e)
@@ -46,7 +40,6 @@ namespace real_time_logger
             UpdateComPortList();  // Populate COM port and baudrate lists
             DatabaseConnection(); // Open database connection
             LoadTableNames();     // Load table names into combo box
-            //PlotData();
         }
 
         private void DatabaseConnection()
@@ -133,7 +126,6 @@ namespace real_time_logger
                         dataY1[i] = Convert.ToDouble(dt.Rows[i]["sensor1"]); // Y1-axis = sensor1 column
                         dataY2[i] = Convert.ToDouble(dt.Rows[i]["sensor2"]); // Y2-axis = sensor2 column
                     }
-
                     PlotDataTable(dataX, dataY1, dataY2);// Plot the data
                 }
             }
@@ -160,27 +152,7 @@ namespace real_time_logger
 
             formsPlot1.Refresh(); // Refresh the plot
         }
-
-        private void PlotData()
-        {
-            // Sample data for testing
-            double[] dataX = { 1, 2, 3, 4, 5 };
-            double[] dataY1 = { 10, 20, 15, 25, 30 };
-            double[] dataY2 = { 5, 15, 10, 20, 25 };
-
-            // Plot sensor1
-            var scatter1 = formsPlot1.Plot.Add.Scatter(dataX, dataY1);
-            scatter1.Color = Colors.Red;
-            scatter1.LineWidth = 2;
-
-            // Plot sensor2
-            var scatter2 = formsPlot1.Plot.Add.Scatter(dataX, dataY2);
-            scatter2.Color = Colors.Blue;
-            scatter2.LineWidth = 2;
-
-            formsPlot1.Refresh(); // Refresh the plot
-        }
-
+        
         public DataTable getData(string query)
         {
             try
@@ -324,7 +296,6 @@ namespace real_time_logger
                 cmd.Parameters.AddWithValue("@lux", lux);
                 cmd.Parameters.AddWithValue("@pot", pot);
                 cmd.Parameters.AddWithValue("@time", DateTime.Now);
-
                 try
                 {
                     cmd.ExecuteNonQuery(); // Execute insert query
