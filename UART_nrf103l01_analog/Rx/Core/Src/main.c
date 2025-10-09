3db870452d21d408b1498454cc15c0d9915f9d85
melike işeri <86420860+melike162@users.noreply.github.com>
2025-10-09 10:44:36 +0300

    Update main.c

--- UART_nrf103l01_analog/Rx/Core/Src/main.c
+++ UART_nrf103l01_analog/Rx/Core/Src/main.c
@@ -81,7 +81,7 @@ uint8_t data_R[PLD_SIZE]={0}; // Receive buffer
 uint8_t ack_R[PLD_SIZE]={0};  // Acknowledge buffer
 #endif
 
-//If not TX, delete******************************************************************
+
 uint8_t convCompleted=0;// Flag indicating ADC conversion completion
 // ADC conversion complete callback
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc)//*****************
